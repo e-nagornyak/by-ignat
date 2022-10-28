3d4cb2c23f11d7461fe2a3ba7ac9a6341ae421a5
e-nagornyak <dolkiv@gmail.com>
2022-10-28 23:31:18 +0300

    Completed HW3 v.1.0.

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -25,7 +25,10 @@ export const pureOnBlur = (name: string, setError: any) => { // если имя 
 
 export const pureOnEnter = (e: KeyboardEvent<HTMLInputElement>, addUser: () => void) => { // если нажата кнопка Enter - добавить
     if (e.key === 'Enter') {
-        addUser()
+        if (e.currentTarget.value.trim() !== '') {
+            console.log(e.currentTarget.value)
+            addUser()
+        }
     }
 }
 
