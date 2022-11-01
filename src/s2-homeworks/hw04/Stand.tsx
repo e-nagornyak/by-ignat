b26729c6a4b57a65de305be4e1d76cbf34f640d0
e-nagornyak <dolkiv@gmail.com>
2022-11-01 22:06:47 +0200

    Test checkbox

--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -96,7 +96,8 @@ const Stand = () => {
                     <SuperCheckbox
                         id={'hw4-super-checkbox-like-old'}
                         checked={stateForAllCheckboxes}
-                        onChange={(e) => setChecked(e.currentTarget.checked)}
+                        onClick={(e) => setChecked(e.currentTarget.checked)}
+                        // onChange={() => alert('Hello')}
                     />
                 </div>
             </div>
